ff6859cb5f484276143f093cece0901b089f4c4b
termorey <is_front@mail.ru>
2023-08-20 16:44:28 +0300

    rename, added export

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,15 @@
 import type {TypedUseSelectorHook} from "react-redux";
 import {useDispatch, useSelector} from "react-redux";
 import {configureStore} from "@reduxjs/toolkit";
-import {postsReducer} from "./posts/postsReducer.ts";
+import {postsSlice} from "./posts/slice.ts";
 
 export const store = configureStore({
 	reducer: {
-		[postsReducer.name]: postsReducer.reducer,
+		[postsSlice.name]: postsSlice.reducer,
 	}
 });
 
-type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof store.getState>;
 type AppDispatch = typeof store.dispatch;
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
